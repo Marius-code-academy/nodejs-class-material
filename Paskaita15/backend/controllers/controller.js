34890aea8bbdf9e88b2b3570e42df2fc63dc619a
Marius <marius_matulevicius@epam.com>
2023-02-27 19:39:27 +0200

    paskaita 15

---
+++ Paskaita15/backend/controllers/controller.js
@@ -0,0 +1,81 @@
+import DB from '../db/db.js';
+// select * from products order by id asc limit 5 offset 10
+export async function getProductsPaginate(req, res) {
+  const { size, page } = req.query;
+  const products = await DB.query(`select * from products order by id asc limit ${size} offset ${(page - 1) * size}`);
+  const count = await DB.query('select count(id) from products');
+  const rowCount = count.rows[0].count;
+
+  const resObj = {
+    totalPages: Math.ceil( rowCount / size),
+    data: products.rows,
+  }
+
+  res.json(resObj);
+}
+
+export async function getAllProducts(req, res) {
+  try {
+    const products = await DB.query('SELECT id, description, price, title FROM products');
+    res.json(products.rows)
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+}
+
+export async function getProductById(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await DB.query(`select id, description, price, title from products where id=${id}`)
+
+    res.json(product.rows[0])
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+}
+
+export async function createNewProduct(req, res) {
+  try {
+    const {
+      title,
+      description,
+      price
+    } = req.body;
+  
+    const product = await DB.query(`insert into products (title, description, price) values ('${title}', '${description}', ${price})`)
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+}
+
+export async function updateProductById(req, res) {
+  try {
+    const { id } = req.params;
+    const {
+      title,
+      description,
+      price
+    } = req.body;
+  
+    const product = await DB.query(`update products set price=${price}, description='${description}', title='${title}' where id=${id}`);
+    res.json({success: true})
+  } catch (error) {
+    res.status(500).json({error: error.message}); 
+  }
+}
+
+export async function deleteProductById(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await DB.query(`delete from products where id=${id}`);
+
+    res.json({success: true})
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+}
+
+export async function routeNotFound(req, res) {
+  res.status(404).json({error: 'Endpoint not found'})
+}
---
+++ Paskaita15/backend/routes/router.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { getAllProducts, createNewProduct, getProductById, updateProductById, deleteProductById, routeNotFound, getProductsPaginate } from '../controllers/controller.js';
+const router = express.Router();
+
+router.get('/products/pagination', getProductsPaginate)
+
+// Pasiimma visus produktus
+router.get('/products', getAllProducts)
+
+// Pasiimam viena produkta pagal ID
+router.get('/products/:id', getProductById)
+
+// sukursim nauja produkta
+router.post('/products', createNewProduct)
+
+// atnaujinsim viena produkta paga ID
+router.put('/products/:id', updateProductById)
+
+// istrinam viena produkta pagal ID
+router.delete('/products/:id', deleteProductById)
+
+router.all('*', routeNotFound)
+
+export default router;
